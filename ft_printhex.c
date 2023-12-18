08adf213361e80f994875c1cc08e1c88b4aac490
sebastianpenning <whimizera@mailbox.org>
2023-12-18 21:07:22 +0100

    ft_printhex norm proof

--- ft_printhex.c
+++ ft_printhex.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printhex.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 21:06:04 by mynodeus          #+#    #+#             */
+/*   Updated: 2023/12/18 21:06:33 by mynodeus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	ft_printhex(va_list args, char specifier)
 {
-	int rtr;
-	unsigned int arg;
-	
+	int				rtr;
+	unsigned int	arg;
+
 	rtr = 0;
 	arg = va_arg(args, unsigned int);
-	if(arg == 0)
+	if (arg == 0)
 	{
 		rtr += ft_putchar_fd('0', 1);
 		return (rtr);
