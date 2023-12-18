d0ff5cb4f3065a29d2ffbfb871f56a7d39d2479b
sebastianpenning <whimizera@mailbox.org>
2023-12-18 21:15:23 +0100

    libftprintf.c normproof

--- libftprintf.c
+++ libftprintf.c
@@ -1,45 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   libftprintf.c                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: spenning <spenning@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/12/03 10:45:10 by spenning      #+#    #+#                 */
-/*   Updated: 2023/12/03 12:41:05 by spenning      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 10:45:10 by spenning          #+#    #+#             */
+/*   Updated: 2023/12/18 21:15:09 by mynodeus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int check_datatype (const char specifier, va_list args)
+static int	check_datatype(const char specifier, va_list args)
 {
-	int rtr;
+	int	rtr;
 
 	rtr = 0;
-	if(specifier == 'c')
+	if (specifier == 'c')
 		rtr = ft_printchar(args);
-	if(specifier == 's')
+	if (specifier == 's')
 		rtr = ft_printstr(args);
-	if(specifier == 'd')
+	if (specifier == 'd')
 		rtr = ft_printint(args);
-	if(specifier == 'i')
+	if (specifier == 'i')
 		rtr = ft_printint(args);
-	if(specifier == 'u')
+	if (specifier == 'u')
 		rtr = ft_printuint(args);
-	if(specifier == '%')
+	if (specifier == '%')
 		rtr = ft_printamp();
-	if(specifier == 'x' || specifier == 'X')
+	if (specifier == 'x' || specifier == 'X')
 		rtr = ft_printhex(args, specifier);
-	if(specifier == 'p')
+	if (specifier == 'p')
 		rtr = ft_printptr(args);
 	return (rtr);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	int rtr;
-	va_list args;
+	int		rtr;
+	va_list	args;
 
 	rtr = 0;
 	va_start(args, format);
@@ -57,5 +57,5 @@ int ft_printf(const char *format, ...)
 		}
 	}
 	va_end(args);
-	return(rtr);
+	return (rtr);
 }
