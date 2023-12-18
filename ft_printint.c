9d7216fb5bafa1551810b88c81d0beddf4ad35f0
sebastianpenning <whimizera@mailbox.org>
2023-12-18 21:08:25 +0100

    ft_printint.c norm proof

--- ft_printint.c
+++ ft_printint.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_printint.c                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: spenning <spenning@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/12/03 10:44:37 by spenning      #+#    #+#                 */
-/*   Updated: 2023/12/03 10:44:38 by spenning      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   ft_printint.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 10:44:37 by spenning          #+#    #+#             */
+/*   Updated: 2023/12/18 21:08:03 by mynodeus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printint(va_list args)
+int	ft_printint(va_list args)
 {
-	int arg;
-	int rtr;
+	int	arg;
+	int	rtr;
 
 	arg = va_arg(args, int);
 	rtr = 0;
