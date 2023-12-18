28eabc4c3d49bc25baf16b737dcb291c5d8e87e4
sebastianpenning <whimizera@mailbox.org>
2023-12-18 21:10:36 +0100

    ft_printstr.c norm proof

--- ft_printstr.c
+++ ft_printstr.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_printstr.c                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: spenning <spenning@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/12/03 10:44:57 by spenning      #+#    #+#                 */
-/*   Updated: 2023/12/03 10:44:58 by spenning      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   ft_printstr.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 10:44:57 by spenning          #+#    #+#             */
+/*   Updated: 2023/12/18 21:10:22 by mynodeus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printstr(va_list args)
+int	ft_printstr(va_list args)
 {
-	int rtr;
-	char *str;
-	
+	int		rtr;
+	char	*str;
+
 	str = va_arg(args, char *);
 	rtr = 0;
-	if(str == NULL)
+	if (str == NULL)
 		rtr = ft_putstr_fd("(null)", 1);
 	else
 	{
