f7db7b11912bbff61f2e9b2bda7e4dc42f24688d
sebastianpenning <whimizera@mailbox.org>
2023-12-18 21:21:04 +0100

    ft_putstr_fd.c normproof

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   ft_putstr_fd.c                                     :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: spenning <spenning@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/10/12 18:44:17 by spenning      #+#    #+#                 */
-/*   Updated: 2023/12/03 12:43:48 by spenning      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 18:44:17 by spenning          #+#    #+#             */
+/*   Updated: 2023/12/18 21:20:47 by mynodeus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	ft_putstr_fd(char *s, int fd)
 {
-	size_t len = ft_strlen(s);
+	size_t	len;
+
+	len = ft_strlen(s);
 	write(fd, s, len);
-	return(len);
+	return (len);
 }
