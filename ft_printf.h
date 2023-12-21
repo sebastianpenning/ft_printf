2316c40ec98d262b043fd03238fc11775f3f3357
spenning <whimizera@mailbox.org>
2023-12-21 13:59:32 +0100

    ft_printf.h docs

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/21 23:03:23 by spenning      #+#    #+#                 */
-/*   Updated: 2023/12/21 13:25:39 by spenning      ########   odam.nl         */
+/*   Updated: 2023/12/21 13:58:10 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,43 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
+// This function prints out the format string + variables 
+// depending on which format specifiers are given 
 int		ft_printf(const char *format, ...);
+// This function aids ft_printf by printing all variables
+// which are passed with the %c formatter. The %c formatter is meant
+// for characters
 int		ft_printchar(va_list args);
+// This function aids ft_printf by printing all variables
+// which are passed with the %s formatter. The %s formatter is meant
+// for string
 int		ft_printstr(va_list args);
+// This function aids ft_printf by printing all variables
+// which are passed with the %i formatter. The %i formatter is meant
+// for signed integers
 int		ft_printint(va_list args);
+// This function aids ft_printf by printing all variables
+// which are passed with the %u formatter. The %u formatter is meant
+// for unsigned integers
 int		ft_printuint(va_list args);
+// This function aids ft_printf by printing all variables
+// which are passed with the %% formatter. The %% formatter is meant
+// for ampersands
 int		ft_printamp(void);
+// This function aids the ft_printf by printing all variables
+// which are passed with either the %x or %X formatter. These formatters are 
+// used to print the hexidecimal values. Which are then passed on to ft_hexconvert
 int		ft_printhex(va_list args, char specifier);
+// This function aids ft_printhex. the values and formatters are passed
+// on to this function. This function recursively converts the value to hexidecimal 
+// and then depending on the formatter will either print the hexidecimal in upper or lowercase
 int		ft_hexconvert(unsigned int arg, char specifier);
-int		ft_hexconvertptr(long unsigned int arg);
+// This function aids ft_printf. by printing all variables
+// which are passed with either the %p formatter.The %p formatter is meant
+// for address of pointers. This function is similar to ft_printhex
 int		ft_printptr(va_list args);
+// This function aids ft_printptr. This function recursively converts the value to hexidecimal 
+// and will then print the hexidecimal lowercase. This function is similar such as ft_hexconvert
+int		ft_hexconvertptr(long unsigned int arg);
 
 #endif
