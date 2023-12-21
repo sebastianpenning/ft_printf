1f5eec6b58a975716da4866db392dca71ccc49f5
spenning <whimizera@mailbox.org>
2023-12-21 14:54:02 +0100

    ft_printhex docs added

--- ft_printhex.c
+++ ft_printhex.c
@@ -1,16 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printhex.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mynodeus <mynodeus@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 21:06:04 by mynodeus          #+#    #+#             */
-/*   Updated: 2023/12/18 21:06:33 by mynodeus         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_printhex.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spenning <spenning@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/18 21:06:04 by mynodeus      #+#    #+#                 */
+/*   Updated: 2023/12/21 14:52:56 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+// this function retrieves the value from va_list
+// and then passes it on t ft_hexconvert to convert the 
+// hexidecimal value
 
 int	ft_printhex(va_list args, char specifier)
 {
